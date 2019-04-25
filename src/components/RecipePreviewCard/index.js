5e5506e1967d37dea6802674bc550f1e4e7e6517
Jake Mitchell <mitchellja97@gmail.com>
2019-04-25 19:24:04 -0400

    fix missing banner

--- src/components/RecipePreviewCard/index.js
+++ src/components/RecipePreviewCard/index.js
@@ -6,10 +6,10 @@ import './style.css';
 class RecipePreviewCard extends Component {
   render() {
     const recipe = this.props.recipe;
-
+    console.log(recipe)
     return (
       <li className="recipePreviewCard">
-        <Banner type="horizontal" image={recipe.images.banner} />
+        <Banner type="horizontal" image="https://images.unsplash.com/photo-1470337458703-46ad1756a187?auto=format&fit=crop&w=2249&q=80" />
         <PreviewContent type="horizontal" recipe={recipe} />
       </li>
     );
